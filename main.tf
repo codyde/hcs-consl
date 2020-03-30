3873e4635abb034b77195e1b25001f0ea579e4b6
Cody De Arkland <cody@hashicorp.com>
2020-03-30 12:34:31 -0700

    Updating with changed intentions

--- main.tf
+++ main.tf
@@ -18,9 +18,3 @@ resource "consul_keys" "app" {
     value = data.template_file.init.rendered
   }
 }
-
-resource "consul_intention" "default-deny" {
-  source_name      = "*"
-  destination_name = "*"
-  action           = "deny"
-}
