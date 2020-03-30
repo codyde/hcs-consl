93fa28598508bdf87a46fa75bb5605209a876470
Cody De Arkland <cody@hashicorp.com>
2020-03-30 16:53:46 -0700

    Baseline packages

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ provider "consul" {
 data "template_file" "init" {
   template = "${file("website.tmpl")}"
   vars = {
-    cardHeader = "HashiCorp Consul Service on Azure - VM <> VM Mesh Demo"
-    cardText = "Our application is designed to demonstrate connectivity between multiple tiers in a Service Mesh running with Consul"
+    cardHeader = ""
+    cardText = ""
   }
 }
 
@@ -23,16 +23,4 @@ resource "consul_intention" "default-deny" {
   source_name      = "*"
   destination_name = "*"
   action           = "deny"
-}
-
-resource "consul_intention" "api-allow" {
-  source_name      = "nginx"
-  destination_name = "pyapi-vm"
-  action           = "allow"
-}
-
-resource "consul_intention" "db-allow" {
-  source_name      = "pyapi-vm"
-  destination_name = "db-vm"
-  action           = "allow"
 }
