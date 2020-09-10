7b7ac91b4ac52c614a283b68aca97394f86be1f7
Cody De Arkland <codydearkland@gmail.com>
2020-09-09 23:58:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,22 @@ resource "consul_config_entry" "frontend" {
     Protocol    = "http"
   })
 }
+
+resource "consul_config_entry" "ingress_gateway" {
+    name = "hcs-igw"
+    kind = "ingress-gateway"
+
+    config_json = jsonencode({
+        Listeners = [{
+            Port     = 8080
+            Protocol = "http"
+            Services = [
+     {
+       Name = "frontend"
+       Hosts = ["*"]
+     }
+   ]
+            
+        }]
+    })
+}
