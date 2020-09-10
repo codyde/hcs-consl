e7cd0d3d63ec8dfb7bb72273e791c155b3a33c78
Cody De Arkland <codydearkland@gmail.com>
2020-09-09 23:37:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ provider "consul" {
   datacenter = var.consuldc
   token      = var.token
 }
+
+resource "consul_intention" "frontend" {
+  source_name      = "frontend"
+  destination_name = "api"
+  action           = "allow"
+}
