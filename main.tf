17fd9772e6b79fbe529155e5582129019454bd2d
Cody De Arkland <codydearkland@gmail.com>
2020-09-25 13:39:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,43 @@ provider "consul" {
   datacenter = var.consuldc
   token      = var.token
 }
+
+resource "consul_intention" "api-allow" {
+  source_name      = "frontend"
+  destination_name = "api"
+  action           = "allow"
+}
+
+resource "consul_intention" "db-allow" {
+  source_name      = "api"
+  destination_name = "db"
+  action           = "allow"
+}
+
+resource "consul_config_entry" "frontend" {
+  name = "frontend"
+  kind = "service-defaults"
+
+  config_json = jsonencode({
+    Protocol    = "http"
+  })
+}
+
+resource "consul_config_entry" "ingress_gateway" {
+    name = "hcs-igw"
+    kind = "ingress-gateway"
+
+    config_json = jsonencode({
+        Listeners = [{
+            Port     = 8080
+            Protocol = "http"
+            Services = [
+     {
+       Name = consul_config_entry.frontend.name
+       Hosts = ["*"]
+     }
+   ]
+            
+        }]
+    })
+}
