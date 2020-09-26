12f7f49256ee0708d3164c9ef001c275664081ad
Cody De Arkland <cody@hashicorp.com>
2020-09-25 22:41:10 -0700

    initial file commit

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ provider "consul" {
   datacenter = var.consuldc
   token      = var.token
 }
+
+resource "consul_namespace" "production" {
+  name        = "production"
+  description = "Production namespace"
+
+ }
