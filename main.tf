6d35713529b8f411706ca96b65c74b3430257d96
Cody De Arkland <cody@hashicorp.com>
2020-09-26 20:09:53 -0700

    adding http defaults for frontend

--- main.tf
+++ main.tf
@@ -33,3 +33,40 @@ resource "consul_config_entry" "frontend-v2" {
         Protocol    = "http"
       })
     }
+
+resource "consul_config_entry" "fe-splitter" {
+    kind = "service-splitter"
+    name = consul_config_entry.frontend.name
+
+    config_json = jsonencode({
+      Splits = [
+    {
+     Weight  = 10
+    },
+    {
+     Weight  = 90
+      Service = "frontend-v2"
+    }
+  ]
+    })
+  }
+
+resource "consul_config_entry" "ingress" {
+    kind = "ingress-gateway"
+    name = "ingress-gateway"
+
+    config_json = jsonencode({
+      Listeners = [
+   {
+     Port = 8080
+     Protocol = "http"
+     Services = [
+       {
+         Name = "frontend"
+         Hosts = ["*"]
+       }
+     ]
+   }
+  ]
+  })
+  }
