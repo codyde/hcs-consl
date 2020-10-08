30ba5443ad2ecea1ce17ac086f8bd43e28cdeec3
Cody De Arkland <cody@hashicorp.com>
2020-10-08 14:30:06 -0700

    initial igw config

--- main.tf
+++ main.tf
@@ -4,3 +4,31 @@ provider "consul" {
   token      = var.token
 }
 
+resource "consul_config_entry" "frontend" {
+    name = "frontend"
+    kind = "service-defaults"
+
+    config_json = jsonencode({
+      Protocol    = "http"
+    })
+  }
+
+resource "consul_config_entry" "ingress" {
+    kind = "ingress-gateway"
+    name = "ingress-gateway"
+
+    config_json = jsonencode({
+      Listeners = [
+   {
+     Port = 8080
+     Protocol = "http"
+     Services = [
+       {
+         Name = "frontend"
+         Hosts = ["*"]
+       }
+     ]
+   }
+  ]
+  })
+  }
